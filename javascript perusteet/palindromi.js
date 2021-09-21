a9fde7fb5a5a2965415172cc5b5bc3ce791fb51b
OonaHar <oonahar@uef.fi>
2021-09-21 21:54:46 +0300

    testi

--- javascript perusteet/palindromi.js
+++ javascript perusteet/palindromi.js
@@ -1,22 +1,12 @@
-function onkopalindromi(str) {
-  var len = str.length;
-  var mid = Math.floor(len / 2);
-
+function palindrome(sana) {
   var input = require("readline-sync");
-
-  for (var i = 0; i < mid; i++) {
-    if (str[i] !== str[len - 1 - i]) {
+  var sana = input.question("Anna sana");
+  var len = sana.length;
+  for (var i = 0; i < Math.floor(len / 2); i++) {
+    if (sana[i] !== sana[len - 1 - i]) {
+      console.log(sana, "ei oo palindromi");
       return false;
     }
   }
-
   return true;
-}
-
-function josPalindromi(str) {
-  var vast = onkopalindromi(String);
-  if ((vast = true)) console.log(input, "On se palindromi");
-}
-if ((vast = false)) {
-  console.log(input, "Ei oo palindromi");
 }
