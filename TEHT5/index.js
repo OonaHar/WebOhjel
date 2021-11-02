88eb9a081da8be8d6b860767cf7cf64415e34f2a
OonaHar <oonahar@uef.fi>
2021-11-02 14:51:38 +0200

    j

--- TEHT5/index.js
+++ TEHT5/index.js
@@ -4,9 +4,9 @@ const mysql = require("mysql");
 // Be sure to replace 'user' and 'password' with the correct values
 const con = mysql.createConnection({
   host: "localhost",
-  user: "mikko",
+  user: "kt",
   password: "kt123456",
-  database: "puhelinluettelo",
+  database: "mysql",
   multipleStatements: true, //out parametria varten aliohjelmassa
 });
 
@@ -89,4 +89,69 @@ con.end((err) => {
   //The connection is terminated gracefully
   //Ensures all remaining queries are executed
   //Then sends a quit packet to the MySQL server.
+});
+
+let data = [
+  { id: 1, firstName: "Matti", lastName: "Ruohonen" },
+  { id: 2, firstName: "Teppo", lastName: "Ruohonen" },
+];
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express().use(bodyParser.json());
+//Â GETÂ allÂ users
+app.get("/users", (req, res) => {
+  res.json(data);
+});
+//Â GETÂ aÂ user
+app.get("/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const user = data.find((user) => user.id === id);
+  res.json(user ? user : { message: "NotÂ found" });
+});
+//Â ADDÂ aÂ user
+app.post("/users", (req, res) => {
+  const user = req.body;
+  data.push(user);
+  res.json(data);
+});
+//Â UPDATEÂ aÂ user
+app.put("/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const updatedUser = req.body;
+  data = data.map((user) => (user.id === id ? updatedUser : user));
+  res.json(data);
+});
+//Â DELETEÂ aÂ user
+app.delete("/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  data = data.filter((user) => user.id !== id);
+  res.json(data);
+});
+app.listen(3306, () => {
+  console.log("ServerÂ listeningÂ atÂ portÂ 3306");
+});
+
+// Add headers
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
 });
